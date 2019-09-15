7d1762a6e2e118cec5a41482e342b66a3b1a87e7
eg0000r <47872540+eg0000r@users.noreply.github.com>
2019-09-15 08:54:21 -0400

    Updates

---
+++ precache-manifest.0307a1dccca7a3c79be876def2d05624.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa3a127ef999a070864f16fd358b476",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "079fb727e6ee540bd132",
+    "url": "/react_app/static/css/main.395379c6.chunk.css"
+  },
+  {
+    "revision": "4b932f42512cfced11e5",
+    "url": "/react_app/static/js/2.c8cdf5db.chunk.js"
+  },
+  {
+    "revision": "079fb727e6ee540bd132",
+    "url": "/react_app/static/js/main.65a6c767.chunk.js"
+  },
+  {
+    "revision": "e4b9c809fd2535394b9b",
+    "url": "/react_app/static/js/runtime~main.b50bfee1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
